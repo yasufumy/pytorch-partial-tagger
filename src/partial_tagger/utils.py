fe9da293e2129d9333e547edff3af472cf309052
yasufumi <yasufumi.taniguchi@gmail.com>
2023-07-19 23:29:00 +0900

    Restore create_tag for backward compatibility

--- src/partial_tagger/utils.py
+++ src/partial_tagger/utils.py
@@ -3,11 +3,26 @@ from __future__ import annotations
 from transformers import AutoTokenizer
 
 from partial_tagger.data.batch.text import TransformerTokenizer
+from partial_tagger.data.core import Span, Tag
 from partial_tagger.encoders.transformer import (
     TransformerModelEncoderFactory,
     TransformerModelWithHeadEncoderFactory,
 )
 from partial_tagger.training import Trainer
+
+
+def create_tag(start: int, length: int, label: str) -> Tag:
+    """Creates a tag.
+
+    Args:
+        start: An integer representing a start index of a tag.
+        length: An integer representing length of a tag.
+        label: A string representing a label of a tag.
+
+    Returns:
+        An instance of Tag.
+    """
+    return Tag(Span(start, length), label)
 
 
 def create_trainer(
